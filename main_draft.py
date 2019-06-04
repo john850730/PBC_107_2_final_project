d91b3c99da496243c3b18d8197fe59ac60e9d87e
Stanley <51200791+StanWorksHardAF@users.noreply.github.com>
2019-06-04 20:12:38 +0800

    main code draft

---
+++ main_draft.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+NTU Grad Game MainFrame
+(Remade version)
+"""
+import pygame#for all the buttons
+import sys#for exit
+import random#for dice
+
+class Player():
+    def __init__(self, ):
+
